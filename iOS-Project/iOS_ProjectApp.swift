08c08e20f09a72ae41a9e65250221d070583a3de
Hjwoon <hjw030705@naver.com>
2025-05-12 01:57:00 +0900

    Begin project

---
+++ iOS-Project/iOS_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ProjectApp.swift
+//  iOS-Project
+//
+//  Created by han on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
